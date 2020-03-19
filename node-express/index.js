182cdf8afea611d77d807e0cc49a28f4959233aa
Abhinav Singh <abhinav.s0612@gmail.com>
2020-03-19 15:58:54 +0530

    express module example

--- node-express/index.js
+++ node-express/index.js
@@ -1,1 +1,19 @@
-console.log("index.js");
+const express = require('express');
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.contentType('Content-Type','text/html');
+    res.end('<html><head></head><body>Welcome</body></html>');
+});
+
+const server = http.createServer(app);
+
+server.listen(port,hostname,()=>{
+console.log(`Server running on hostname : ${hostname} on port : ${port}`);
+});
