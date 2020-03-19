0b407ad9d5e4e236c6a501ab79da877964d050a8
Abhinav Singh <abhinav.s0612@gmail.com>
2020-03-19 17:29:36 +0530

    added endpoints

--- node-express/index.js
+++ node-express/index.js
@@ -1,12 +1,61 @@
 const express = require('express');
 const morgan = require('morgan');
 const http = require('http');
+const bodyParser = require('body-parser');
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes',(req,res,next)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type',"text/plain")
+    next();
+});
+
+app.get('/dishes',(req,res,next)=>{
+    res.end('All the dished will be sent to you');
+});
+
+app.put('/dishes',(req,res,next)=>{
+    res.statusCode = 403;
+    res.end('Invalid request');
+});
+
+app.post('/dishes',(req,res,next)=>{
+    res.end('Dish '+req.body.name +'with details : '+req.body.description +' will be added.');
+});
+
+app.delete('/dishes',(req,res,next)=>{
+    res.end('All the dished will be deleted');
+});
+// dish with param dishId
+
+app.get('/dishes/:dishId',(req,res,next)=>{
+    res.end('Dish ' +req.params.dishId + 'will be sent to you');
+});
+
+app.put('/dishes/:dishId',(req,res,next)=>{
+   res.write('Updating dish '+ req.params.dishId);
+   res.end('Dish with id '+req.params.dishId +' updated.');
+});
+
+app.post('/dishes/:dishId',(req,res,next)=>{
+    res.statusCode = 403;
+    res.end('Invalid request');
+});
+
+app.delete('/dishes/:dishId',(req,res,next)=>{
+    res.end('Dish '+req.params.dishId+ 'will be deleted');
+});
+
+
+
+
 
 app.use(express.static(__dirname+'/public'));
 app.use((req,res,next)=>{
